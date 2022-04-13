a8d55364c396eb1e64e5e9fe48e89b050450e39f
Ken Marsh <kenskyfish@gmail.com>
2022-04-13 11:15:44 +0700

    working plugin

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+function hello(action, settings) {
+  return new Promise((resolve, reject) => {
+    const name = action.params.helloName || settings.helloName;
+    const saySecret = (action.params.saySecret && action.params.secret);
+    const secret = saySecret ? action.params.secret : undefined;
+    if (!name) {
+      reject(new Error("No name was given."));
+    }
+    let greeting = `Hello ${name}.`;
+    if (secret) {
+      greeting += ` Here is the secret: ${secret}`;
+    }
+    resolve(greeting);
+  });
 }
 
 module.exports = {
