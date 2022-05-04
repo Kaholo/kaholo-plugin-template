f0cd17965265dd102c3cc411e8d19585e7c3187e
Ken Marsh <kenny.m@kaholo.io>
2022-05-04 15:54:07 +0800

    refactored a little for readability

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ async function hello(action, settings) {
   const name = action.params.helloName || settings.helloName;
   const saySecret = action.params.saySecret || undefined;
   const secret = action.params.secret || undefined;
+
   if (!name) {
     throw new Error("No name was given.");
   }
+  
   let greeting = `Hello ${name}.`;
-  if (saySecret) {
-    if (!secret) {
+  
+  if (saySecret && !secret) {
       throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
-    }
+  }
+
+  if (saySecret) {
     greeting += ` Here is the secret: ${secret}`;
   }
+
   return greeting;
 }
 
