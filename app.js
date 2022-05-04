5c5e0975254de59cfca1baa63262062441b59463
Ken Marsh <kenny.m@kaholo.io>
2022-05-04 15:17:20 +0800

    updated a bit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-function hello(action, settings) {
-  return new Promise((resolve, reject) => {
-    const name = action.params.helloName || settings.helloName;
-    const saySecret = (action.params.saySecret && action.params.secret);
-    const secret = saySecret ? action.params.secret : undefined;
-    if (!name) {
-      reject(new Error("No name was given."));
+async function hello(action, settings) {
+  const name = action.params.helloName || settings.helloName;
+  const saySecret = action.params.saySecret || undefined;
+  const secret = action.params.secret || undefined;
+  if (!name) {
+    throw new Error("No name was given.");
+  }
+  let greeting = `Hello ${name}.`;
+  if (saySecret) {
+    if (!secret) {
+      throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
     }
-    let greeting = `Hello ${name}.`;
-    if (secret) {
-      greeting += ` Here is the secret: ${secret}`;
-    }
-    resolve(greeting);
-  });
+    greeting += ` Here is the secret: ${secret}`;
+  }
+  return greeting;
 }
 
 module.exports = {
