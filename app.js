fdea51ddcf7016be0af8b5979e30b27fa8e6dd5f
Kenny Marsh <kenny.m@kaholo.io>
2022-07-29 11:44:59 +0200

    [KP-871] Make template a Kaholo plugin that actually works

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
+async function hello(action, settings) {
+  const name = action.params.helloName || "there";
+  const saySecret = action.params.saySecret || undefined;
+  const secret = action.params.secret || undefined;
+
+  let greeting = `Hello ${name}!`;
+
+  if (saySecret && !secret) {
+    throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
+  }
+
+  if (saySecret) {
+    greeting += `\nHere is the secret: ${secret}`;
+  }
+
   return greeting;
 }
 
