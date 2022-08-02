58eb5d4c8ad362cc06959689edd30ba791d6b679
Kenny Marsh <kenny.m@kaholo.io>
2022-08-02 15:46:03 +0200

    changed as suggested

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const { bootstrap } = require("@kaholo/plugin-library");
 
 async function hello(params) {
-  const name = params.helloName || "there";
-  const saySecret = params.saySecret || undefined;
-  const secret = params.secret || undefined;
 
-  let greeting = `Hello ${name}!`;
+  const {
+    helloName,
+    saySecret,
+    secret,
+  } = params;
+
+  let greeting = `Hello ${helloName}!`;
 
   if (saySecret && !secret) {
     throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
