171bfc6289554a3592574e543b648f60012fb82f
Ken Marsh <10334573+kenskyfish@users.noreply.github.com>
2022-08-03 12:50:00 +0200

    [KP-871] Make template a Kaholo plugin that actually works (#18)

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
+const { bootstrap } = require("@kaholo/plugin-library");
+
+async function hello(params) {
+  const {
+    helloName,
+    saySecret,
+    secret,
+  } = params;
+
+  let greeting = `Hello ${helloName}!`;
+
+  if (saySecret && !secret) {
+    throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
+  }
+
+  if (saySecret) {
+    greeting += `\nHere is the secret: ${secret}`;
+  }
+
   return greeting;
 }
 
-module.exports = {
+module.exports = bootstrap({
   hello,
-};
+});
